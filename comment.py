ca9ac2c1abad4fada082024f759cdf7ea2a87f80
Exitcode <danielbomban1@gmail.com>
2026-02-11 12:20:39 +0100

    Add files via upload
    Additional small projects from Chapter 2. However I might want to update favorite_number.py once I learn how to output with a set delay to create suspense in the message I am trying to communicate in the program :)
    
    In this Chapter 2 I learned about using variables, what integers and floats are, and how to use strings and f-strings. I also learned a little about how to use numbers, how to use comments, and also how to keep my Python code as simple as I possibly can. 
    
    I love how easy and engaging it is to follow Eric Matthes in his book Python Crash Course.

---
+++ comment.py
@@ -0,0 +1,3 @@
+### This is a comment, and it might be useful to describe how I am solving certain problems.
+### Python interpreter ignores this, so this is a way to communicate with anyone reading my code.
+print("While I am a human I can read robot.")
