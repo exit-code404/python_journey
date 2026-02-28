5e0fc8552885033e4fae35cce929ee2d19cbc102
exit-code404 <danielbomban1@gmail.com>
2026-02-28 13:22:45 +0100

    Learned more about if-elif-else statements, and updated small refinements to existing conditional_tests file.

--- Chapter 5/conditional_tests.py
+++ Chapter 5/conditional_tests.py
@@ -28,40 +28,36 @@ if user in range(0, 18):
     print("Minors are not allowed entry!")
 elif user in range(18, 65):
     print("Adults have access with an entry fee of $50")
-elif user in range(65, 150):
+else:
     print("Seniors have free access!")
 
 print(f"\n --- \n")
 
 # Grade Calculator
 
-user_score = 95
+user_score = 55
 
 if user_score in range(90, 101):
     grade = 'A'
-    print(f"You scored {user_score}, which means you passed with grade {grade}!")
 if user_score in range(70, 90):
     grade = 'B'
-    print(f"You scored {user_score}, which means you passed with grade {grade}!")
 if user_score in range(50, 70):
     grade = 'C'
-    print(f"You scored {user_score}, which means you passed with grade {grade}!")
 if user_score in range(30, 50):
     grade = 'D'
-    print(f"You scored {user_score}, which means you passed with grade {grade}!")
 if user_score in range(15, 30):
     grade = 'E'
-    print(f"You scored {user_score}, which means you passed with grade {grade}!")
 if user_score in range(0, 15):
     grade = 'F'
-    print(f"You scored {user_score}, which means you passed with grade {grade}!")
+
+print(f"You scored {user_score}, which means you passed with grade {grade}!")    
 
 print(f"\n --- \n")
 
 # Triangle Classifier
 
 a = 15
-b = 12
+b = 15
 c = 15    
 
 ## Determining Triangle Validity
@@ -86,7 +82,7 @@ else:
 
 if sum(valid) == 3:
     print(f"\nThis is a valid Triangle because the result of valid conditions was: {sum(valid)}")
-if sum(invalid) == 1 or sum(invalid) == 2 or sum(invalid) == 3:
+if sum(invalid) == 1:
     print(f"This is an invalid Triangle because the result of invalid conditions was: {sum(invalid)}, while valid conditions was {sum(valid)}")    
 
 ## Classifying the Triangle
