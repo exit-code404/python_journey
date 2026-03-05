e6d088e38ca27620508f482d4f8c27f9619cf5df
exit-code404 <danielbomban1@gmail.com>
2026-03-05 21:39:43 +0100

    Beginning of a new chapter 6 which is about dictionaries

---
+++ Chapter 5/my_ideas.py
@@ -0,0 +1,2 @@
+ideas = ['password strength analyzer', 'port scanner', 'packet sniffer', 'subnet calculator', 
+         'file integrity monitor', 'brute-force login simulator', 'network device discovery tool']
