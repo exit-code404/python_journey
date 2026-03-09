b41aca93f6ff2ba7e82572d2afc3604f0ee4cb20
exit-code404 <danielbomban1@gmail.com>
2026-03-09 17:39:54 +0100

    I was probably suppose to access the key and the value in the dictionary manually one-by-one, however I just jumped straight into finding out how I could loop through it by using a for loop and the new method items().

---
+++ Chapter 6/favorite_numbers.py
@@ -0,0 +1,11 @@
+fav_numb = {
+    'daniel': '6',
+    'anne': '2',
+    'anna': '19',
+    'torgeir': '14',
+    'rune': '9',
+    'herdis': '30',
+}
+
+for key, value in fav_numb.items():
+    print(f"{key.title()}: {value}")
---
+++ Chapter 6/glossary.py
@@ -0,0 +1,10 @@
+glossary = {
+    "for": "Python's way of a loop",
+    "if": "Conditional statement",
+    "else": "If 'if' does not match, then do the else statement",
+    "shuffle": "Choose a random item on the list or dictionary",
+    "import": "Imports additional features"
+}
+
+for key, value in glossary.items():
+    print(f"The word: '{key}' has the definition: '{value}'.")
