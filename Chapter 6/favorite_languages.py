614b9a10273c75255b5da18dd6a901e01f6b41d0
exit-code404 <danielbomban1@gmail.com>
2026-03-16 16:50:17 +0100

    If those in many_users.py had unique key items, then the code would be much more complicated than that. I guess then I would have to figure out how to generate each key item from each dictionary to find all items inside it. Unique or not.

--- Chapter 6/favorite_languages.py
+++ Chapter 6/favorite_languages.py
@@ -1,29 +1,15 @@
 favorite_language = {
-    'daniel': 'python',
-    'emilie': 'c',
-    'noemi': 'rust',
-    'katya': 'python',
+    'daniel': ['python', 'rust'],
+    'emilie': ['c'],
+    'noemi': ['rust', 'go'],
+    'katya': ['python', 'ruby'],
 }
 
-friends = ['emilie', 'noemi']
-for name in sorted(favorite_language.keys()):
-    print(f"Hi {name.title()}.")
-
-    if name in friends:
-        language = favorite_language[name].title()
-        print(f"\t{name.title()}, I see you love {language}")
-
-if 'dan' not in favorite_language.keys():
-    print("\nDan please take the poll!")
-
-print("\nThe following languages have been mentioned in the poll:")
-for language in set(favorite_language.values()):
-    print(language.title())
-
-advise_poll = ['rune', 'anne', 'lek', 'noemi', 'katya', 'herdis', 'eli', 'anna']
-for name in advise_poll:
-    if name in favorite_language:
-        print(f"Thank you, {name.title()} for taking the poll!")
-
-    if name not in favorite_language:
-        print(f"Since you have not taken our poll yet, we strongly advise you to do so {name.title()}.")    
+for name, languages in favorite_language.items():
+    if len(languages) > 1:
+        print(f"\n{name.title()}'s favorite languages are:")
+        for language in languages:
+            print(f"\t{language.title()}")
+    elif len(languages) == 1:
+        print(f"\n{name.title()}'s favorite language is {languages[0].title()}.")
+            
