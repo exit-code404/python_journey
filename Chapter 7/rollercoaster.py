66bfdb73abd8065c76da2a7c262003fa8a491146
exit-code404 <danielbomban1@gmail.com>
2026-03-26 20:23:33 +0100

    The modulo operator is quite facinating. I do get it, but I still do not fully understand how someone could think of a logic that is logic using that modulo operator to get even or odd numbers. That is interesting.

---
+++ Chapter 7/rollercoaster.py
@@ -0,0 +1,7 @@
+height = input("Enter your height in centimeters: ")
+height = int(height)
+
+if height >= 140:
+    print("\nYou are tall enough to ride!")
+else:
+    print("\nUnfortunatly you will have to wait a few years before you can ride.")
