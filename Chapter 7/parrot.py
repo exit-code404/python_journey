9fd52c2045bfcdfee1cb50a126bb54afdf7b6895
exit-code404 <danielbomban1@gmail.com>
2026-03-29 18:40:41 +0200

    while loops introduction, and the concept as of now is pretty straightforward.

--- Chapter 7/parrot.py
+++ Chapter 7/parrot.py
@@ -1,2 +1,9 @@
-message = input("Tell me something very interesting, and I will repeat it back to you ")
-print(message)
+prompt = "\nTell me something very interesting, and I will repeat it back to you"
+prompt += "\nEnter 'quit to end the program. "
+
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    
+    if message != 'quit':
+        print(message)
