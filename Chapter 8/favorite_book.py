caeb30b2a5c40e2e7b0a47fcb05ae83deb2bdb3d
exit-code404 <danielbomban1@gmail.com>
2026-04-11 23:13:35 +0200

    introduction to functions

---
+++ Chapter 8/favorite_book.py
@@ -0,0 +1,4 @@
+def favorite_book(title):
+    print(f"One of my most fantastic reads was {title.title()}")
+
+favorite_book('the zen trader')    
---
+++ Chapter 8/message.py
@@ -0,0 +1,7 @@
+def display_message():
+    '''Display a message'''
+    prompt = "\nIn chapter 8 - I am learning about Functions."
+    prompt += "\nIt should help me write better code that is easier to build, run, test, and debug."
+    print(prompt)
+
+display_message()
