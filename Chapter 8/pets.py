f88ec19ed04cdd04a2b00b08eadd5399bd57a171
exit-code404 <danielbomban1@gmail.com>
2026-04-12 22:41:46 +0200

    postional arguments and keyword arguments

--- Chapter 8/pets.py
+++ Chapter 8/pets.py
@@ -1,6 +1,8 @@
-def describe_pet(animal_type, pet_name):
+def describe_pet(pet_name, animal_type='cat'):
     """Display information about a pet"""
     print(f"\nI have a {animal_type}.")
     print(f"\nMy {animal_type}'s name is {pet_name.title()}.")
 
-describe_pet('cat', 'pus')
+describe_pet('pus')
+describe_pet(animal_type='dog', pet_name='cane corso')
+
---
+++ Chapter 8/t_shirt.py
@@ -0,0 +1,7 @@
+def make_shirt(size="large", text="I love Python!"):
+    '''Make a T Shirt with text'''
+    print(f"This T-Shirt is size {size} with the printed text: {text}")
+
+make_shirt()
+make_shirt(size="medium")
+make_shirt(size="small", text="Illustionist")    
