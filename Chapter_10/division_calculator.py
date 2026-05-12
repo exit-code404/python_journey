7e78aaae3825749cf3856d9a70248470c52ab958
exit-code404 <danielbomban1@gmail.com>
2026-05-12 09:34:06 +0200

    Try-Except Blocks Handling errors without showing tracebacks that could allow users more information than I would like to happen which would cause security issues.

---
+++ Chapter_10/division_calculator.py
@@ -0,0 +1,18 @@
+print("Give me two numbers and I will divide them.")
+print("Press 'q' to quit the program")
+
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+
+    second_number = input("\nSecond number: ")
+    if second_number == 'q':
+        break
+
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print("You can't divide by zero!")
+    else:
+        print(answer)    
